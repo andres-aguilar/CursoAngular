6959807a95874ce728f7a23f0420af5e097ae32b
Andres Aguilar <andresyoshimar@gmail.com>
2018-06-07 22:38:01 -0500

    Creando animacion de entrada escalonada para la lista de los repos

---
+++ src/app/animations/staggeredShowUp.animation.ts
@@ -0,0 +1,16 @@
+import { trigger, style, transition, animate, stagger, query, keyframes } from '@angular/animations';
+
+export const staggeredShowUp = trigger('staggeredShowUp', [
+  transition('* => *', [
+    // Primero ocultamos todos
+    query(':enter', style({transform: 'translateX(-100px)', opacity: '0'})),
+    // Aplicamos una apoaricion escalonada
+    query(':enter', stagger('80ms', [
+      animate('0.4s ease-out', keyframes([
+        style({transform: 'translateX(-100px)', opacity: '0', offset: 0}),
+        style({transform: 'translateX(-50px)', opacity: '0.5', offset: 0.5}),
+        style({transform: 'translateX(0px)', opacity: '1', offset: 1})
+      ]))
+    ]), {optional: true})
+  ])
+])
--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 import { ArticlesService } from '../services/articles.service';
 import { showUp } from '../animations/showUp.animation';
+import { staggeredShowUp } from '../animations/staggeredShowUp.animation';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss'],
-  animations: [ showUp ]
+  animations: [ showUp, staggeredShowUp ]
 })
 export class ArticlesComponent implements OnInit {
 
